a70acc7c354f07ac497c055298976ebbd649d2a8
Dick Verweij <d.verweij@afas.nl>
2025-01-15 14:41:54 +0100

    Update ManageCalendarEventsPlugin.java

--- android/src/main/java/com/fantastic/manage_calendar_events/ManageCalendarEventsPlugin.java
+++ android/src/main/java/com/fantastic/manage_calendar_events/ManageCalendarEventsPlugin.java
@@ -22,7 +22,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /**
  * ManageCalendarEventsPlugin
@@ -63,13 +62,14 @@ public class ManageCalendarEventsPlugin implements FlutterPlugin, ActivityAware,
         methodChannel.setMethodCallHandler(null);
     }
 
-    /**
-     * Plugin registration.
-     */
-    public static void registerWith(Registrar registrar) {
-        Context context = registrar.context();
-        Activity activity = registrar.activity();
-        setup(new ManageCalendarEventsPlugin(), registrar.messenger(), activity, context);
+    @Override
+    public void onAttachedToActivity(final ActivityPluginBinding binding) {
+        this.activityBinding = binding;
+        this.setup(
+                this.pluginBinding.getBinaryMessenger(),
+                (Application) this.pluginBinding.getApplicationContext(),
+                this.activityBinding.getActivity(),
+                this.activityBinding);
     }
 
 
